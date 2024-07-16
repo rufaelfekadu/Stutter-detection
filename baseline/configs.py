48fb39c6d78a1636e8e7a9b8b5aa3b943f9d35f1
rufaelfekadu <rufaelfekadu48@gmail.com>
2024-07-16 10:29:47 +0000

    updated configuration

--- baseline/configs.py
+++ baseline/configs.py
@@ -7,13 +7,20 @@ _C.name = 'stutter-detection'
 _C.seed = 42
 _C.tasks = ['t1', 't2']
 
+
+# logging and saving
+_C.output = CN()
+_C.output.save_dir = '../outputs'
+_C.output.log_dir = 'logs'
+_C.output.checkpoint_dir = 'checkpoints' 
+
 # dataset configs
 _C.data = CN()
 
 _C.data.name = 'sep28k'
 _C.data.root = 'datasets/sep28k/clips'
 _C.data.label_path = 'datasets/sep28k/SEP-28k_labels_new.csv'
-_C.data.ckpt = '../outputs/sep28k/sep28k.pt'
+_C.data.ckpt = 'dataset'
 
 # data loader configs
 
@@ -27,7 +34,6 @@ _C.model.num_layers = 1
 _C.model.output_size = 6
 _C.model.dropout = 0.5
 _C.model.seq_len = 301
-_C.model.num_classes = 6
 
 # convlstm
 _C.model.emb_dim = 64
@@ -54,8 +60,4 @@ _C.solver.log_interval = 10
 _C.solver.lr_step = 10
 _C.solver.lr_decay = 0.1
 
-# logging and saving
-_C.output = CN()
-_C.output.log_dir = '../outputs/logs'
-_C.output.save_dir = '../outputs/checkpoints'
 
