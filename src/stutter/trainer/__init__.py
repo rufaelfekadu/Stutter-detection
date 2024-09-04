91c115d5eb6054a07b86537fc6cb5ed82367d0e4
rufaelfekadu <rufaelfekadu48@gmail.com>
2024-09-04 15:45:08 +0000

    trainer inint update

--- src/stutter/trainer/__init__.py
+++ src/stutter/trainer/__init__.py
@@ -1,1 +1,14 @@
-from .trainer import build_trainer
+from .trainer import *
+from .multimodal import MultiModalTrainer
+
+trainer_registery = {
+    'mtl': MTLTrainer,
+    'yoho': SedTrainer,
+    'sed': SedTrainer2,
+    'vivit': VivitTrainer,
+    'multimodal': MultiModalTrainer
+}
+
+def build_trainer(cfg, logger=None, metrics=['f1']):
+    trainer = trainer_registery[cfg.setting](cfg, logger, metrics)
+    return trainer
