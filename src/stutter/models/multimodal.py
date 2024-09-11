b74d7a2a1b2f4b7c93cc5f586b4cd01edc9ae1b2
rufaelfekadu <rufaelfekadu48@gmail.com>
2024-09-11 12:21:10 +0000

    multimodal training

--- src/stutter/models/multimodal.py
+++ src/stutter/models/multimodal.py
@@ -5,117 +5,296 @@ import torch.nn as nn
 from typing import Optional
 
 
-CACHE_DIR="/tmp/"
+CACHE_DIR="outputs/"
 _HIDDEN_STATES_START_POSITION = 2
 
-class AudioExtractor(Wav2Vec2ForSequenceClassification):
-  def __init__(self, config):
-    super().__init__(config)
-    self.freeze_base_model()
-    self.wav2vec2.feature_extractor.requires_grad_(True)
-    self.projector.requires_grad_(True)
-  def forward(
-      self,
-      input_values,
-      attention_mask=None,
-      output_attentions=None,
-      output_hidden_states=None,
-      return_dict=None,
-  ):
-    output_hidden_states = True if self.config.use_weighted_layer_sum else output_hidden_states
 
-    outputs = self.wav2vec2(
-        input_values,
-        attention_mask=attention_mask,
-        output_attentions=output_attentions,
-        output_hidden_states=output_hidden_states,
-        return_dict=return_dict,
-    )
-    if self.config.use_weighted_layer_sum:
-        hidden_states = outputs[_HIDDEN_STATES_START_POSITION]
-        hidden_states = torch.stack(hidden_states, dim=1)
-        norm_weights = nn.functional.softmax(self.layer_weights, dim=-1)
-        hidden_states = (hidden_states * norm_weights.view(-1, 1, 1)).sum(dim=1)
-    else:
-        hidden_states = outputs[0]
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-    hidden_states = self.projector(hidden_states)
-    if attention_mask is None:
-        pooled_output = hidden_states.mean(dim=1)
-    else:
-        padding_mask = self._get_feature_vector_attention_mask(hidden_states.shape[1], attention_mask)
-        hidden_states[~padding_mask] = 0.0
-        pooled_output = hidden_states.sum(dim=1) / padding_mask.sum(dim=1).view(-1, 1)
-    return pooled_output
+class Residual(nn.Module):
+
+    def __init__(self, in_channels, num_hiddens, num_residual_hiddens, use_kaiming_normal):
+        super(Residual, self).__init__()
+        
+        relu_1 = nn.ReLU(True)
+        conv_1 = nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=num_residual_hiddens,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias=False
+        )
+        if use_kaiming_normal:
+            conv_1 = nn.utils.weight_norm(conv_1)
+            nn.init.kaiming_normal_(conv_1.weight)
+
+        relu_2 = nn.ReLU(True)
+        conv_2 = nn.Conv1d(
+            in_channels=num_residual_hiddens,
+            out_channels=num_hiddens,
+            kernel_size=1,
+            stride=1,
+            bias=False
+        )
+        if use_kaiming_normal:
+            conv_2 = nn.utils.weight_norm(conv_2)
+            nn.init.kaiming_normal_(conv_2.weight)
+
+        # All parameters same as specified in the paper
+        self._block = nn.Sequential(
+            relu_1,
+            conv_1,
+            relu_2,
+            conv_2
+        )
+    
+    def forward(self, x):
+        return x + self._block(x)
+
+class ResidualStack(nn.Module):
+
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens, use_kaiming_normal):
+        super(ResidualStack, self).__init__()
+        
+        self._num_residual_layers = num_residual_layers
+        self._layers = nn.ModuleList(
+            [Residual(in_channels, num_hiddens, num_residual_hiddens, use_kaiming_normal)] * self._num_residual_layers)
+        
+    def forward(self, x):
+        for i in range(self._num_residual_layers):
+            x = self._layers[i](x)
+        return F.relu(x)
+
+class Conv1DBuilder(object):
+
+    @staticmethod
+    def build(in_channels, out_channels, kernel_size, stride=1, padding=0, use_kaiming_normal=False):
+        conv = nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding
+        )
+        if use_kaiming_normal:
+            conv = nn.utils.weight_norm(conv)
+            nn.init.kaiming_normal_(conv.weight)
+        return conv
+
+class ConvolutionalEncoder(nn.Module):
+    '''
+        Taken from: https://github.com/swasun/VQ-VAE-Speech
+    '''
+    
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens,
+        use_kaiming_normal, input_features_type, features_filters, sampling_rate,
+        device, verbose=False):
+
+        super(ConvolutionalEncoder, self).__init__()
+
+        """
+        2 preprocessing convolution layers with filter length 3
+        and residual connections.
+        """
+
+        self._conv_1 = Conv1DBuilder.build(
+            in_channels=features_filters,
+            out_channels=num_hiddens,
+            kernel_size=3,
+            use_kaiming_normal=use_kaiming_normal,
+            padding=1
+        )
+
+        self._conv_2 = Conv1DBuilder.build(
+            in_channels=num_hiddens,
+            out_channels=num_hiddens,
+            kernel_size=3,
+            use_kaiming_normal=use_kaiming_normal,
+            padding=1
+        )
+
+        """
+        1 strided convolution length reduction layer with filter
+        length 4 and stride 2 (downsampling the signal by a factor
+        of two).
+        """
+        self._conv_3 = Conv1DBuilder.build(
+            in_channels=num_hiddens,
+            out_channels=num_hiddens,
+            kernel_size=4,
+            stride=2, # timestep * 2
+            use_kaiming_normal=use_kaiming_normal,
+            padding=2
+        )
+
+        """
+        2 convolutional layers with length 3 and
+        residual connections.
+        """
+
+        self._conv_4 = Conv1DBuilder.build(
+            in_channels=num_hiddens,
+            out_channels=num_hiddens,
+            kernel_size=3,
+            use_kaiming_normal=use_kaiming_normal,
+            padding=1
+        )
+
+        self._conv_5 = Conv1DBuilder.build(
+            in_channels=num_hiddens,
+            out_channels=num_hiddens,
+            kernel_size=3,
+            use_kaiming_normal=use_kaiming_normal,
+            padding=1
+        )
+
+        """
+        4 feedforward ReLu layers with residual connections.
+        """
+
+        self._residual_stack = ResidualStack(
+            in_channels=num_hiddens,
+            num_hiddens=num_hiddens,
+            num_residual_layers=num_residual_layers,
+            num_residual_hiddens=num_residual_hiddens,
+            use_kaiming_normal=use_kaiming_normal
+        )
+        
+        self._input_features_type = input_features_type
+        self._features_filters = features_filters
+        self._sampling_rate = sampling_rate
+        self._device = device
+        self._verbose = verbose
+
+    def forward(self, inputs):
+
+        x_conv_1 = F.relu(self._conv_1(inputs))
+        x = F.relu(self._conv_2(x_conv_1)) + x_conv_1
+        x_conv_3 = F.relu(self._conv_3(x))
+        x_conv_4 = F.relu(self._conv_4(x_conv_3)) + x_conv_3
+        x_conv_5 = F.relu(self._conv_5(x_conv_4)) + x_conv_4
+        x = self._residual_stack(x_conv_5) + x_conv_5
+
+        return x
+
+class AudioExtractor(nn.Module):
+    __acceptable_params__ = ['input_size', 'hidden_size', 'num_layers', 'output_size', 'dropout']
+    def __init__(self, **kwargs):
+        super(AudioExtractor, self).__init__()
+        [setattr(self, k, v) for k, v in kwargs.items() if k in self.__acceptable_params__]
+        
+        # self.emb = nn.Linear(self.input_size, self.hidden_size)
+        self.lstm1 = nn.LSTM(self.input_size, self.hidden_size, num_layers=1, batch_first=True, dropout=self.dropout)
+        self.lstm2 = nn.LSTM(self.hidden_size, 32, num_layers=1, batch_first=True, dropout=self.dropout)
+        self.ln = nn.LayerNorm(32)
+        self.emb = nn.Linear(32, self.output_size)
+        
+        
+    def forward(self, x: torch.Tensor, tasks=['t1', 't2']):
+        # x->(batch_size, seq_len, input_size)
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+
+        out, _ = self.lstm1(x, (h0, c0))
+        out, _ = self.lstm2(out)
+
+        out = self.ln(out[:,-1,:])
+        
+        return self.emb(out)
 
 class VideoExtractor(VivitForVideoClassification):
-  def __init__(self, config):
-    super().__init__(config)
-    self.vivit.requires_grad_(False)
-    self.vivit.embeddings.requires_grad_(True)
-    self.vivit.encoder.layer[0].requires_grad_(True)
-    self.vivit.encoder.layer[11].requires_grad_(True)
-    self.classifier.requires_grad_(True)
-    
-  def forward(
-        self,
-        pixel_values: Optional[torch.FloatTensor] = None,
-        head_mask: Optional[torch.FloatTensor] = None,
-        labels: Optional[torch.LongTensor] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        interpolate_pos_encoding: bool = False,
-        return_dict: Optional[bool] = None,
-    ):
-    outputs = self.vivit(
-        pixel_values,
-        head_mask=head_mask,
-        output_attentions=output_attentions,
-        output_hidden_states=output_hidden_states,
-        interpolate_pos_encoding=interpolate_pos_encoding,
-        return_dict=return_dict,
-    )
+    def __init__(self, config):
+        super().__init__(config)
+        self.vivit.requires_grad_(False)
+        self.vivit.embeddings.requires_grad_(True)
+        self.vivit.encoder.layer[0].requires_grad_(True)
+        self.vivit.encoder.layer[11].requires_grad_(True)
+        self.classifier.requires_grad_(True)
+        self.emb = nn.Linear(768, 128)
+        
+    def forward(
+            self,
+            pixel_values: Optional[torch.FloatTensor] = None,
+            head_mask: Optional[torch.FloatTensor] = None,
+            labels: Optional[torch.LongTensor] = None,
+            output_attentions: Optional[bool] = None,
+            output_hidden_states: Optional[bool] = None,
+            interpolate_pos_encoding: bool = False,
+            return_dict: Optional[bool] = None,
+        ):
+        outputs = self.vivit(
+            pixel_values,
+            head_mask=head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            interpolate_pos_encoding=interpolate_pos_encoding,
+            return_dict=return_dict,
+        )
 
-    sequence_output = outputs[0][:, 0, :]
-    return sequence_output
+        sequence_output = outputs[0][:, 0, :]
+        return sequence_output
 
 class MultiModalClassification(nn.Module):
-    def __init__(self, num_labels, wav2vec2_config, vivit_config):
-      super().__init__()
-      self.num_labels = num_labels
-      self.audio_extractor  = AudioExtractor.from_pretrained("superb/wav2vec2-base-superb-ks",config=wav2vec2_config, cache_dir=CACHE_DIR)
-      self.video_extractor = VideoExtractor.from_pretrained("google/vivit-b-16x2-kinetics400", config=vivit_config, ignore_mismatched_sizes=True, cache_dir=CACHE_DIR)
-      self.activation = nn.GELU()
-      self.projector = nn.Linear(1024, 1024)
-      self.classifier = nn.Linear(1024, self.num_labels)
-      self.audio_layer_norm = nn.LayerNorm(256)
-      self.video_layer_norm = nn.LayerNorm(768)
-      self.multi_norm = nn.BatchNorm1d(1024)
+    __acceptable_params__ = ['output_size', 'vid_num_frames', 'vid_size']
+    def __init__(self, **kwargs):
+        super().__init__()
+        [setattr(self, k, kwargs.get(k, None)) for k, v in kwargs.items() if k in self.__acceptable_params__]
+
+        self.audio_extractor  = AudioExtractor(input_size=40, hidden_size=64, num_layers=1, output_size=128, dropout=0.5)
+        vivit_config = VivitConfig.from_pretrained("google/vivit-b-16x2-kinetics400")
+        vivit_config.num_frames=self.vid_num_frames
+        vivit_config.video_size=self.vid_size
+        self.video_extractor = VideoExtractor.from_pretrained("google/vivit-b-16x2-kinetics400", config=vivit_config, ignore_mismatched_sizes=True, cache_dir=CACHE_DIR)
+        self.activation = nn.GELU()
+        self.projector = nn.Linear(256, 128)
+        self.classifier = nn.Linear(128, self.output_size)
+        self.audio_layer_norm = nn.LayerNorm(128)
+        self.video_layer_norm = nn.LayerNorm(128)
+        self.multi_norm = nn.BatchNorm1d(128)
+      
 
       
     def forward(self, pixel_values, input_values, attention_mask, labels):
-      audio = self.audio_extractor(input_values,attention_mask)
-      audio = self.audio_layer_norm(audio)
-      video = self.video_extractor(pixel_values)
-      video = self.video_layer_norm(video)
-      x = torch.cat((video, audio), dim=1)
-      output = self.projector(x)
-      output = self.multi_norm(output)
-      logits = self.classifier(self.activation(output))
-      
-      loss = None
-      # if labels is not None:
-      #   loss_fct = CrossEntropyLoss()
-      #   loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
-      if labels is not None and self.num_labels == 1:
-          logits = torch.sigmoid(logits).view(-1)
-          loss_ce = torch.nn.BCELoss()
-          loss = loss_ce(logits, labels)
-      else:
-          pos_weight = torch.tensor([1349/185, 1384/150, 1481/53, 1225/309, 1134/500,1354/180, 924/610 , 1346/188]).to(labels.device)
-          loss_fct = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)
-          loss = loss_fct(logits.view(-1, self.num_labels), 
-                          labels.float().view(-1, self.num_labels))
+        audio = self.audio_extractor(input_values,attention_mask)
+        audio = self.audio_layer_norm(audio)
+        video = self.video_extractor(pixel_values)
+        video = self.video_layer_norm(video)
+        x = torch.cat((video, audio), dim=1)
+        output = self.projector(x)
+        output = self.multi_norm(output)
+        logits = self.classifier(self.activation(output))
+
+        loss = None
+        # if labels is not None:
+        #   loss_fct = CrossEntropyLoss()
+        #   loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+        if labels is not None and self.out_putsize == 1:
+            logits = torch.sigmoid(logits).view(-1)
+            loss_ce = torch.nn.BCELoss()
+            loss = loss_ce(logits, labels)
+        else:
+            pos_weight = torch.tensor([1349/185, 1384/150, 1481/53, 1225/309, 1134/500,1354/180, 924/610 , 1346/188]).to(labels.device)
+            loss_fct = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+            loss = loss_fct(logits.view(-1, self.out_putsize), 
+                            labels.float().view(-1, self.out_putsize))
+
+        return (loss, logits)
         
-      return (loss, logits)
-    
+
+if __name__ == "__main__":
+
+    kwargs = {
+        'output_size': 6,
+        'vid_num_frames': 10,
+        'vid_size': [10, 224, 224]
+    }
+    model = MultiModalClassification(**kwargs)
+    print(model)
+    audio_input = torch.randn(32, 301, 40)
+    video_input = torch.randn(32, 10, 3, 224, 224)
+    labels = torch.randint(0, 2, (32, 6))
+    loss, logits = model(video_input, audio_input, None, labels)
+
--- src/stutter/trainer/multimodal.py
+++ src/stutter/trainer/multimodal.py
@@ -4,7 +4,7 @@ from .trainer import Trainer
 from stutter.models.multimodal import MultiModalClassification
 from stutter.utils.meters import AverageMeter
 from transformers import TrainingArguments, Wav2Vec2Config, VivitConfig
-from datasets import load_from_disk, Dataset, concatenate_datasets, load_metric
+from datasets import load_from_disk, Dataset, concatenate_datasets, load_metric, load_dataset
 import torch
 import torch.nn.functional as F
 from tqdm import tqdm
@@ -26,35 +26,12 @@ def collate_fn(batch):
 
 class MultiModalTrainer(Trainer):
 
-    def _init_meters(self):
-        self.val_meters['accuracy'] = AverageMeter('val_accuracy', writer=self.logger)
-        self.val_meters['f1_weighted'] = AverageMeter('val_f1_weighted', writer=self.logger)
-        self.val_meters['f1_macro'] = AverageMeter('val_f1_macro', writer=self.logger)
-        self.val_meters['f1_any'] = AverageMeter('val_f1_any', writer=self.logger)
-        self.val_meters['lr'] = AverageMeter('lr', writer=self.logger)
-        self.val_meters['loss'] = AverageMeter('val_loss', writer=self.logger)
-        
-        for classes in STUTTER_CLASSES:
-            self.val_meters[classes] = AverageMeter(f'val_{classes}_f1', writer=self.logger)
-        
-        self.train_meters['loss'] = AverageMeter('train_loss', writer=self.logger)
-
-        self.test_meters['accuracy'] = AverageMeter('test_accuracy', writer=self.logger)
-        self.test_meters['f1_weighted'] = AverageMeter('test_f1_weighted', writer=self.logger)
-        self.test_meters['f1_macro'] = AverageMeter('test_f1_macro', writer=self.logger)
-        self.test_meters['f1_any'] = AverageMeter('test_f1_any', writer=self.logger)
-        self.test_meters['loss'] = AverageMeter('test_loss', writer=self.logger)
-        for classes in STUTTER_CLASSES:
-            self.test_meters[classes] = AverageMeter(f'test_{classes}_f1', writer=self.logger)
-
     def get_model(self):
         # TODO: Fix this function
-        wavconfig = Wav2Vec2Config.from_pretrained("superb/wav2vec2-base-superb-ks")
-        vivitconfig = VivitConfig.from_pretrained("google/vivit-b-16x2-kinetics400")
-        vivitconfig.num_frames=10
-        vivitconfig.video_size=[10,224,224]
+        # vivitconfig = VivitConfig.from_pretrained("google/vivit-b-16x2-kinetics400")
+        
 
-        model = MultiModalClassification(self.cfg.model.output_size, wavconfig, vivitconfig)
+        model = MultiModalClassification(self.cfg.model.output_size)
         optim = torch.optim.SGD(model.parameters(), lr=self.cfg.solver.lr)
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, T_max=10, eta_min=5e-6)
 
@@ -64,10 +41,12 @@ class MultiModalTrainer(Trainer):
         # TODO: Fix this function
         # df = load_from_disk("outputs/fluencybank/dataset/stutter_hf/ds_5_multimodal_train")
         # test_dataset = load_from_disk("outputs/fluencybank/dataset/stutter_hf/label_split/Gold_multimodal_test")
-        df = load_from_disk(f"{self.cfg.data.label_path}train")
-        test_dataset = load_from_disk("outputs/fluencybank/dataset/stutter_hf/label_split/Gold_multimodal_test")
+        # df = load_dataset(f'herwoww/{self.cfg.data.annotator}_multimodal_train')
+        # test_dataset = load_dataset(f'herwoww/Gold_multimodal_train')
+        df = load_dataset(f"herwoww/{self.cfg.data.annotator}_multimodal_train", cache_dir="/tmp/").remove_columns(['input_values', 'attention_mask'])['train']
+        test_dataset=load_dataset("herwoww/Gold_multimodal_train", cache_dir="/tmp/").remove_columns(['input_values', 'attention_mask'])['train']
         
-        if self.cfg.tasks[0] == 't1':
+        if self.tasks[0] == 't1':
             if self.cfg.data.annotation != 'any':
                 ann_index = STUTTER_CLASSES.index(self.cfg.data.annotation)
                 print(f"Label is {self.cfg.data.annotation} at index {ann_index}")
@@ -140,7 +119,7 @@ class MultiModalTrainer(Trainer):
     
     def parse_batch_train(self, batch):
         image = batch['pixel_values'].to(self.device)
-        audio = batch['input_values'].squeeze(1).to(self.device)
+        audio = batch['input_values'].to(self.device)
         attention_mask = batch['attention_mask'].squeeze(1).to(self.device)
         if self.cfg.tasks[0] == 't1':
             y = batch['labels'].to(self.device).float()
@@ -171,113 +150,17 @@ class MultiModalTrainer(Trainer):
     def test_step(self, batch):
         image, audio, attention_mask, y = self.parse_batch_train(batch)
         loss, logits = self.model(pixel_values=image, input_values=audio, attention_mask=attention_mask, labels=y)
-        return loss, logits, y
-    
-    # def after_test(self):
-    #     preds = torch.cat(self.preds, axis=0)
-    #     labels = torch.cat(self.labels, axis=0).cpu().numpy()
-    #     preds = (preds.sigmoid() > 0.5).cpu().numpy()
-    #     cm = confusion_matrix(labels, preds)
-    #     disp = ConfusionMatrixDisplay(confusion_matrix=cm).plot()
-    #     # log the confusion matrix
-    #     # self.logger.add_figure(f'confusion_matrix', disp.figure_)
-    #     for i, s in enumerate(STUTTER_CLASSES):
-    #         cm = confusion_matrix(labels[:,i], preds[:,i])
-    #         disp = ConfusionMatrixDisplay(confusion_matrix=cm).plot()
-    #         # log the confusion matrix
-    #         self.logger.add_figure(f'confusion_matrix_{s}', disp.figure_)
-    
-    def validate(self):
-        self.stage = 'val'
-        self.model.eval()
-        self._reset_meters(self.val_meters)
 
-        with torch.no_grad():
-            total_loss = 0
-            for batch in self.val_loader:
-                losses = self.val_step(batch)
-                total_loss += losses['loss']
-                for key, loss in losses.items():
-                    self._update_meter(self.val_meters, f'{key}', loss)
-
-            
-            # log the learning rate
-            self.val_meters['lr'].update(self.scheduler.get_last_lr()[0])
-            if isinstance(self.scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
-                self.scheduler.step(total_loss/len(self.val_loader))
-            else:
-                self.scheduler.step()
-
-            self._write_meters(self.val_meters)
-            total_loss /= len(self.val_loader)
-
-            if total_loss < self.best_val_loss:
-                self.best_val_loss = total_loss
-                self.best_epoch = self.epoch
-                self.save_model('best_checkpoint.pt')
-                self.patience = self.cfg.solver.es_patience
-                print(f"Saving best model at epoch {self.epoch} with loss {self.best_val_loss}")
-            else:
-                self.patience -= 1
-                if self.patience == 0:
-                    print(f"Early Stopping at epoch {self.epoch} \nbest epoch at {self.best_epoch} with loss {self.best_val_loss}")
-                    return True
-        
-        # self.after_validation()
-        self.model.train()
-        return False
-    # def test(self, loader=None, name='test'):
-    #     loader = loader or self.test_loader
-    #     self.stage = 'test'
-    #     # self.load_model()
-    #     self.model.eval()
-    #     self._reset_meters(self.test_meters)
-    #     self.preds = []
-    #     self.labels = []    
-    #     with torch.no_grad():
-    #         for batch in tqdm(loader, desc='Evaluating Test Set', total=len(loader)):
-    #             loss, logits, labels = self.test_step(batch)
-    #             self.preds.append(logits)
-    #             self.labels.append(labels)
-    #     logits = torch.cat(self.preds, axis=0)
-    #     # del self.preds
-    #     y = torch.cat(self.labels, axis=0)              
-    #     # del self.labels 
-    #     metrics = self.compute_metrics(logits.cpu(), y.cpu())
-    #     metrics['loss'] = loss.item()
-    #     for key_metric, val in metrics.items():
-    #         print(key_metric, val)
-    #         self._update_meter(self.test_meters, f'{key_metric}', val)
-            
-    #     self._write_meters(self.test_meters)
-    #     self.after_test()
-    
-    def test(self, loader=None, name='test'):
-        loader = loader or self.test_loader
-        self.stage = 'test'
-        # self.load_model()
-        self.model.eval()
-        self._reset_meters(self.test_meters)
-        self.preds = []
-        self.labels = []    
-        with torch.no_grad():
-            for batch in tqdm(loader, desc='Evaluating Test Set', total=len(loader)):
-                loss, logits, labels = self.test_step(batch)
-                self.preds.append(logits)
-                self.labels.append(labels)
-        logits = torch.cat(self.preds, axis=0)
-        del self.preds
-        y = torch.cat(self.labels, axis=0)              
-        del self.labels 
         if self.cfg.tasks[0] == 't1':
             metrics = self.compute_t1_metrics(logits, y.cpu())
             metrics['loss'] = loss.item()
         else:
             metrics = self.compute_t2_metrics(logits.cpu(), y.cpu())
             metrics['loss'] = loss.item()
-        for key_metric, val in metrics.items():
-            print(key_metric, val)
-            self._update_meter(self.test_meters, f'{key_metric}', val)
-            
-        self._write_meters(self.test_meters)
-        self.after_test()
+        
+        return {
+            'loss': loss.item(),
+            **metrics
+        }
+    
+    
